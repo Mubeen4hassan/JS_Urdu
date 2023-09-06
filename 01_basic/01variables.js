ad6499345c2b36d86d0dd920dd0fa08fee57cf6f
Mubeen Asghar <mubeen4hassan@gmail.com>
2023-09-06 02:26:07 +0000

    Configratu]ion

---
+++ 01_basic/01variables.js
@@ -0,0 +1,16 @@
+const id= 3119143
+let Email="mubeen4hassani@gmail.com"
+var age=20
+city="Gujranwala"
+let state;
+Email="hassan@gamil.com"
+age=19
+city="Lahore"
+
+// id=120220  const val not changed 
+console.log(id);
+console.table([id,Email,age,city,state]);
+
+/* prefer not to use 
+ because of scope of issue in scope 
+ & functional scope*/
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,7 @@
+// "use strict"; // treat all js code as newer code
+// alert(3+3) we are using node js not browser
+
+let name="hassan"
+age=19
+console.log(typeof(name));
+
